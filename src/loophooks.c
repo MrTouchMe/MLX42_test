ef7960b3b725039e3cc76c6bd014c58be08810a7
Deian-Marian Giurgev <dgiurgev@1-A-4.42heilbronn.de>
2024-03-16 16:32:44 +0100

    wall colision is not FUCKED anymore

--- src/loophooks.c
+++ src/loophooks.c
@@ -39,26 +39,63 @@
 // 		image->instances[0].x += 5;
 // }
 //
+// void ft_hook(mlx_key_data_t keydata, void* param) {
+// 	t_map *map = (t_map*)param;
+
+//  	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
+// 		mlx_close_window(map->mlx);
+
+// 	if (keydata.key == MLX_KEY_W && keydata.action == MLX_PRESS
+// 		&& map->map[(int)map->player->instances[0].x / TILE][((int)map->player->instances[0].y / TILE) - 1] == '0')
+// 		ft_move(map, 0, -1);
+
+
+// 	if (keydata.key == MLX_KEY_A && keydata.action == MLX_PRESS
+// 		&& map->map[((int)map->player->instances[0].x / TILE - 1)][(int)map->player->instances[0].y / TILE] == '0')
+// 		ft_move(map, -1, 0);
+
+// 	if (keydata.key == MLX_KEY_S && keydata.action == MLX_PRESS
+// 		&& map->map[(int)map->player->instances[0].x / TILE][((int)map->player->instances[0].y / TILE) + 1] == '0')
+// 		ft_move(map, 0, 1);
+
+// 	if (keydata.key == MLX_KEY_D && keydata.action == MLX_PRESS
+// 		&& map->map[((int)map->player->instances[0].x / TILE) + 1][(int)map->player->instances[0].y / TILE] == '0')
+// 		ft_move(map, 1, 0);
+// }
+
 void ft_hook(mlx_key_data_t keydata, void* param) {
-	t_map *map = (t_map*)param;
+    t_map *map = (t_map*)param;
 
- 	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
-		mlx_close_window(map->mlx);
+    if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
+        mlx_close_window(map->mlx);
 
-	if (keydata.key == MLX_KEY_W && keydata.action == MLX_PRESS
-		&& map->map[(int)map->player->instances[0].x / TILE][((int)map->player->instances[0].y / TILE) - 1] == '0')
-		ft_move(map, 0, -1);
+    int new_x, new_y;
 
+    if (keydata.key == MLX_KEY_W && keydata.action == MLX_PRESS) {
+        new_x = map->player->instances[0].x / TILE;
+        new_y = (map->player->instances[0].y - TILE) / TILE;
+        if (map->map[new_y][new_x] == '0')
+            ft_move(map, 0, -1);
+    }
 
-	if (keydata.key == MLX_KEY_A && keydata.action == MLX_PRESS
-		&& map->map[((int)map->player->instances[0].x / TILE - 1)][(int)map->player->instances[0].y / TILE] == '0')
-		ft_move(map, -1, 0);
+    if (keydata.key == MLX_KEY_A && keydata.action == MLX_PRESS) {
+        new_x = (map->player->instances[0].x - TILE) / TILE;
+        new_y = map->player->instances[0].y / TILE;
+        if (map->map[new_y][new_x] == '0')
+            ft_move(map, -1, 0);
+    }
 
-	if (keydata.key == MLX_KEY_S && keydata.action == MLX_PRESS
-		&& map->map[(int)map->player->instances[0].x / TILE][((int)map->player->instances[0].y / TILE) + 1] == '0')
-		ft_move(map, 0, 1);
+    if (keydata.key == MLX_KEY_S && keydata.action == MLX_PRESS) {
+        new_x = map->player->instances[0].x / TILE;
+        new_y = (map->player->instances[0].y + TILE) / TILE;
+        if (map->map[new_y][new_x] == '0')
+            ft_move(map, 0, 1);
+    }
 
-	if (keydata.key == MLX_KEY_D && keydata.action == MLX_PRESS
-		&& map->map[((int)map->player->instances[0].x / TILE) + 1][(int)map->player->instances[0].y / TILE] == '0')
-		ft_move(map, 1, 0);
+    if (keydata.key == MLX_KEY_D && keydata.action == MLX_PRESS) {
+        new_x = (map->player->instances[0].x + TILE) / TILE;
+        new_y = map->player->instances[0].y / TILE;
+        if (map->map[new_y][new_x] == '0')
+            ft_move(map, 1, 0);
+    }
 }
